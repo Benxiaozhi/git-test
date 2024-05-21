8a918a22c34832019a5817e672c3a9a376648e60
Benxiaozhi <1807141253@qq.com>
2024-05-21 21:48:53 +0800

    master  44444commit

--- src/main/java/com/benxiaozhi/git/gitTest.java
+++ src/main/java/com/benxiaozhi/git/gitTest.java
@@ -6,5 +6,6 @@ public class gitTest {
         System.out.println("hello ,git22222222");
         System.out.println("hello ,git33333333333333333");
         System.out.println("hello ,git444444444444444444444444444");
+        System.out.println("hello ,git=master11111");
     }
 }
