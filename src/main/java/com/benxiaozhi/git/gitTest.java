209da00b037310e61ddbdfc4f93fa7f5c172aae6
Echo <61101415+Benxiaozhi@users.noreply.github.com>
2024-05-21 23:38:27 +0800

    Update gitTest.java
    pull commit

--- src/main/java/com/benxiaozhi/git/gitTest.java
+++ src/main/java/com/benxiaozhi/git/gitTest.java
@@ -9,5 +9,6 @@ public class gitTest {
         System.out.println("hello ,git=master11111");
         System.out.println("hello ,hot-fix");
         System.out.println("hello ,push test");
+        System.out.println("hello ,pull test");
     }
 }
